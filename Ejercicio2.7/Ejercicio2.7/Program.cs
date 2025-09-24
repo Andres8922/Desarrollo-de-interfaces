c94ce698ba02debb3599cd671c7243b69fa0c319
Andres8922 <andresgarciagomez892@gmail.com>
2025-09-24 14:38:33 +0200

    Add files via upload

---
+++ Ejercicio2.7/Ejercicio2.7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2._7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingrese el tamaño del array: ");
+            int tamaño = int.Parse(Console.ReadLine());
+
+            int[] array = new int[tamaño];
+
+            
+            Console.WriteLine("Ingrese los elementos del array: ");
+
+                for (int i = 0; i < tamaño; i++){
+
+                    Console.Write("Elemento " +(i + 1)+": ");
+
+                    array[i] = int.Parse(Console.ReadLine()); //Leo el número introducido y lo meto en la posición i del array
+                }
+
+
+           Console.WriteLine("Los números pares e impares son: ");
+            for (int j = 0; j < array.Length; j++)
+            {
+               
+                int num = array[j];
+
+                if (num % 2 == 0)
+                {
+                    Console.WriteLine("El valor "+num + " es par");
+                }
+
+                else
+                {
+                    Console.WriteLine("El valor "+num + " es impar");
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Ejercicio2.8/Ejercicio2.8/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random();
+
+            int[] array = new int[10];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(1, 101);
+            }
+
+            Console.WriteLine("Array generado: ");
+
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine();
+
+            //Ordeno el array y lo muestro
+            Console.WriteLine("Array ordenado: ");
+
+            Array.Sort(array);
+
+            foreach (int num in array)
+            {
+                Console.Write(num + " ");
+            }
+
+
+        }
+    }
+}
