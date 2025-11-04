54dd1f5c8470667c725ab168969fc6e962c12d8e
Andres8922 <andresgarciagomez892@gmail.com>
2025-11-04 21:42:39 +0100

    Add files via upload

---
+++ Practica2_WPF/Ejercicio1/Ejercicio8/Ejercicio8/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void todos_Checked(object sender, RoutedEventArgs e)
+        {
+
+                D.IsChecked = true;
+                A.IsChecked = true;
+                S.IsChecked = true;
+            
+        }
+
+        private void todos_Unchecked(object sender, RoutedEventArgs e)
+        {
+            D.IsChecked = false;
+            A.IsChecked = false;
+            S.IsChecked = false;
+        }
+    }
+}
