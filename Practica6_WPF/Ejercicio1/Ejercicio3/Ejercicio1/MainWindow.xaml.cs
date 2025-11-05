c3bdb9beff59a53b096d99535ccffb1fb32168c0
Andres8922 <andresgarciagomez892@gmail.com>
2025-11-05 14:39:54 +0100

    Add files via upload

---
+++ Practica6_WPF/Ejercicio1/Ejercicio3/Ejercicio1/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Label_AccessKeyPressed(object sender, AccessKeyPressedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (txtNombre.Text.Length > 0 && txtEmail.Text.Length > 0 && txtComentario.Text.Length > 0)
+            {
+                
+                MessageBox.Show("El usuario " + txtNombre.Text + " con email " + txtEmail.Text +" dice "+txtComentario.Text, "Información", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+
+            }
+
+        }
+    }
+}
