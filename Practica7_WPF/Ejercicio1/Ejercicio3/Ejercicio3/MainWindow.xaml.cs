805cbcb73f7491efb03313b64861bd142b5c595f
Andres8922 <andresgarciagomez892@gmail.com>
2025-11-11 13:21:04 +0100

    Add files via upload

---
+++ Practica7_WPF/Ejercicio1/Ejercicio3/Ejercicio3/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CargarTxt(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos de texto(*.txt) | *.txt";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                txtEditor.Text= File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void GuardarTxt(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = "txt";
+            saveFileDialog.AddExtension = true;
+            if (saveFileDialog.ShowDialog()==true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, txtEditor.Text);    
+            }
+        }
+    }
+}
