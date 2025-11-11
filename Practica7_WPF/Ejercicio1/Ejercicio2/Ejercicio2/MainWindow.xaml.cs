bdd5d0b5d27d2023444b4b53dff3abdbd82eb4a4
Andres8922 <andresgarciagomez892@gmail.com>
2025-11-11 13:20:20 +0100

    Add files via upload

---
+++ Practica7_WPF/Ejercicio1/Ejercicio2/Ejercicio2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void CargarImagen_Click(object sender, RoutedEventArgs e)
+        {
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Archivos de imagen|*.png;*.jpg;*.jpeg";
+
+            if (openFileDialog.ShowDialog() == true)
+            { 
+                BitmapImage bitmap = new BitmapImage(new Uri(openFileDialog.FileName));
+                imagenMostrar.Source = bitmap;
+                
+            }
+        }
+    }
+}
