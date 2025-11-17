695503897280403932c48d2502dad11afb9e1410
Andres8922 <andresgarciagomez892@gmail.com>
2025-11-17 10:52:29 +0100

    Add files via upload

---
+++ Practica8_WPF/Ejercicio5/Ejercicio5/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("El contenido es " + txtbox.Text, "Mostrar", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        //Cortar
+        private void CommandCut_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (txtbox != null && txtbox.SelectionLength > 0)
+            {
+                e.CanExecute = true;   
+            }
+        }
+
+        private void CommandCut_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            txtbox.Cut();
+        }
+
+        //Copiar
+        private void CommandCopy_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (txtbox != null && txtbox.SelectionLength > 0)
+            {
+                e.CanExecute = true;
+            }
+        }
+
+        private void Copy_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            txtbox.Copy();
+        }
+
+        //Pegar
+        private void CommandPaste_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (txtbox != null && txtbox.SelectionLength > 0)
+            {
+                e.CanExecute = true;
+            }
+        }
+
+        private void Paste_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            txtbox.Paste();
+        }
+    }
+}
