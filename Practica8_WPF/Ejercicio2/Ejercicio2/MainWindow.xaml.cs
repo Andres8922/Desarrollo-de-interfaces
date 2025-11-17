f5589cc538ad7ec4c46711eff5584b7fb70845a6
Andres8922 <andresgarciagomez892@gmail.com>
2025-11-17 10:49:31 +0100

    Add files via upload

---
+++ Practica8_WPF/Ejercicio2/Ejercicio2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Usuario_click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Se ha elegido la opción de Usuario");
+        }
+
+        private void Saldo_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Se ha elegido la opción de Saldo");
+        }
+
+        private void Salir_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Se ha elegido la opción de Salir");
+        }
+
+
+        private void Sumar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Se ha elegido la opción de Sumar");
+        }
+
+        private void Restar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Se ha elegido la opción de Restar");
+        }
+    }
+}
